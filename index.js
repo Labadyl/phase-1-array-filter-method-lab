e173a381b17d2108bcd1dd5bbc41b10b04758656
Lesley Labady <labadylesley@gmail.com>
2022-07-10 20:55:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(driver, string){
+    return driver.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+function fuzzyMatch(driver, string){
+    return driver.filter(driver => driver.toLowerCase().substring(0, string.length) === string.toLowerCase())
+}
+function matchName(driver, hometown){
+    return driver.filter(driver => driver.name === hometown)
+}
